42ce5fb6da9b5af3b40fa0df68cb83b640e45f97
Karl Augsten <karlaugsten@gmail.com>
2014-11-09 23:16:19 -0700

    Add fast tank differential movement

--- Strategies/differential_movement_strategy.cpp
+++ Strategies/differential_movement_strategy.cpp
@@ -8,7 +8,14 @@ double DifferentialMovementStrategy::CalculateGoodness(GameState &state, double 
     goodness -= 1.0/(y*y);
     goodness -= 1.0/((x - state.map.width)*(x - state.map.width));
     goodness -= 1.0/((y - state.map.height)*(y - state.map.height));
-
+    if(state.opponent.alive) {
+        if(state.opponent.TankSlow.alive) {
+            goodness -= (1.0 / (state.opponent.TankSlow.position.Distance(Position(x, y))));
+        }
+        if(state.opponent.TankFast.alive){
+            goodness -= (1.0 / (state.opponent.TankFast.position.Distance(Position(x, y))));
+        }
+    }
     // TODO: Add in calculation for terrain object walls
     // TODO: Add in calculation for projectiles
     return goodness;
@@ -26,7 +33,7 @@ std::queue<Command*> DifferentialMovementStrategy::DetermineActions(GameState &s
     if(!state.player.alive) return moves;
     if(state.player.TankSlow.alive) {
         // for now do a linear search for where the goodness is increasing with radius 1.
-        double r = 1.0;
+        double r = 0.1;
         Position curPos = state.player.TankSlow.position;
         double mx = 1E30;
         double my = 1E30;
@@ -58,7 +65,37 @@ std::queue<Command*> DifferentialMovementStrategy::DetermineActions(GameState &s
         moves.push(new MoveCommand(1.0, state.player.TankSlow.id));
     }
     if(state.player.TankFast.alive) {
-
+        // for now do a linear search for where the goodness is increasing with radius 1.
+        double r = 0.1;
+        Position curPos = state.player.TankFast.position;
+        double mx = 1E30;
+        double my = 1E30;
+        double maxgoodness = -1E30;
+        double currentgoodness = CalculateGoodness(state, curPos);
+        for(int t = 0; t < 1000; t++){
+            double dx = r*cos(2*acos(-1)/double(t));
+            double dy = r*sin(2*acos(-1)/double(t));
+            double goodness = CalculateGoodness(state, curPos.x+dx, curPos.y+dy);
+            if(goodness - currentgoodness > maxgoodness){
+                maxgoodness = goodness - currentgoodness;
+                mx = dx;
+                my = dy;
+            }
+        }
+        // best position is now in mx, my;
+        Position bestPos = Position(curPos.x + mx, curPos.y + my);
+        double angle = state.player.TankFast.tracks;
+        double tmp = curPos.GetAngle(bestPos) - angle;
+        if (tmp > acos(-1)) {
+            tmp -= 2 * acos(-1);
+        }
+        if (tmp < -acos(-1)) {
+            tmp += 2 * acos(-1);
+        }
+        angle = tmp;
+        // TODO: Check for unbreakable terrain
+        moves.push(new RotateCommand(angle, state.player.TankFast.id));
+        moves.push(new MoveCommand(1.0, state.player.TankFast.id));
     }
 
     return moves;
